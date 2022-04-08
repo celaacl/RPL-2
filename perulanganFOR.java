816a07913a45f369210521e82dac598a2aa4bdcb
cecell <91251956+celaacl@users.noreply.github.com>
2022-04-08 08:38:42 +0700

    Add files via upload

---
+++ perulanganFOR.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class perulanganFOR {
+
+    
+        public static void main(String args[]){
+            int x;
+            for(x=1;x<10;x++){
+                System.out.println("Nilai X "+x);
+            }
+        }
+     
+    
+}
